8825d9b34e1af9a3d4a8ca13f8e0fd08402bfb5b
dinesh <dnshshrestha@gmail.com>
2012-06-20 15:34:02 +0545

    changes in js file

--- newsfeed.js
+++ newsfeed.js
@@ -1,50 +1,4 @@
-<<<<<<< HEAD
-/*$(document).ready(function(){
-  $("button").click(function()
-  {
-    $("p").css("color","green");
-  });
-});
-*/
 
-/*$(document).ready(function($) {
-
-  var allPanels = $('.accordion > p').hide();
-
-$(document).ready(function($) {
-
- /* var allPanels = $('.accordion > p').hide();
-
-  $('.accordion > h3 > a').click(function() {
-    allPanels.slideUp();
-    $(this).parent().next().slideDown();
-    return false;
-  });
-*/
-
-$(document).ready(function() {
-	var uri = 'http://services.digg.com/2.0/topics.getAll';
-	var params = {
-					  	  type : 'json'
-								};
-	$.getJSON(uri, params, function(json) {
-  	  if (json.topics.length > 0) {
-  	      for (i in json.topics) {
-  	          $('.topics').append('<li>' + json.topics[i].name + '</li>');
-  		        alert("dasfaf "+json.topics[i].name);
-  	      }
-
-        //$('#stories').show('slow');
-    }
-		});
-
-});
-
-
-});
-
-=======
->>>>>>> develop
 $.ajax({
   type: 'GET',
   url: 'http://services.digg.com/2.0/digg.getAll?type=javascript&callback=?',
@@ -54,6 +8,20 @@ $.ajax({
     {
         $(".topics").append("<li><a href='#'>"+data.diggs[i].item.topic.name+'.html'"</a></li>");
         //$(".topics").append("<li>"+data.diggs[i].item.topic+"</li><br>");
+        $(".topics").append("<li>"+data.diggs[i].item.description+"</li><br>");
+   
+    };
+  }
+});
+
+$.ajax({
+  type: 'GET',
+  url: 'http://services.digg.com/2.0/newsroom.getNews?type=javascript&newsroom_name=technology&callback=?',
+  dataType: 'jsonp',
+  success: function(data){
+    for (i = 0; i < data.count; i++)
+    {
+        $(".topics").append("<li>"+data.diggs[i].item.topic+"</li><br>");
         $(".topics").append("<li>"+data.diggs[i].item.description+"</li><br>");
    
     };
