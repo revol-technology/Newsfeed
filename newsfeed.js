adc43353f6a55066ff93d5b2c5abbe37c2e80c5b
dinesh <dnshshrestha@gmail.com>
2012-06-20 13:05:52 +0545

    changes in newsfeed

--- newsfeed.js
+++ newsfeed.js
@@ -1,31 +1,14 @@
-/*$(document).ready(function(){
-  $("button").click(function()
-  {
-    $("p").css("color","green");
-  });
-});
-*/
-
-/*$(document).ready(function($) {
-    
-  var allPanels = $('.accordion > p').hide();
-    
-  $('.accordion > h3 > a').click(function() {
-    allPanels.slideUp();
-    $(this).parent().next().slideDown();
-    return false;
-  });
-
-});*/
-
 $.ajax({
   type: 'GET',
-  url: 'http://services.digg.com/2.0/topic.getAll?type=javascript&callback=?',
+  url: 'http://services.digg.com/2.0/digg.getAll?type=javascript&callback=?',
   dataType: 'jsonp',
   success: function(data){
     for (i = 0; i < data.count; i++)
     {
-        $(".topics").append("<li>"+data.topics[i].name+"</li>");
-    }
+        $(".topics").append("<li><a href='#'>"+data.diggs[i].item.topic.name+'.html'"</a></li>");
+        //$(".topics").append("<li>"+data.diggs[i].item.topic+"</li><br>");
+        $(".topics").append("<li>"+data.diggs[i].item.description+"</li><br>");
+   
+    };
   }
 });
